06ced30278a34f3048fe697e000630e7cfff16bb
algor <k>
2015-01-03 20:12:45 +0200

    added more main page styles

--- scripts/js/custom.js
+++ scripts/js/custom.js
@@ -1,10 +1,35 @@
 $(function() {
+
+    $('.up-button').hide();
+
     $('#mainpage').fullpage({
         //Navigation
 //        menu: true,
 //        anchors:['1', '2','3'],
-        navigation: true,
-        navigationPosition: 'right'
+//        navigation: true,
+//        navigationPosition: 'right',
+        resize: false,
 
+        afterLoad : function(anchorLink, index){
+            switch (index) {
+                case 3 :
+                    $('.down-button').fadeOut();
+                    break;
+                case 1 :
+                    $('.up-button').fadeOut();
+                    break;
+                default:
+                    $('.up-button').show();
+                    $('.down-button').show();
+            }
+        }
     });
+
+    $('.up-button').on('click', function(){
+        $.fn.fullpage.moveSectionUp();
+    });
+
+    $('.down-button').on('click', function(){
+        $.fn.fullpage.moveSectionDown();
+    })
 });
